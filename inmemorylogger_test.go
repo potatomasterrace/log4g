a38011f42dfdd36004b798f83c1a62f664849491
potatomasterrace <reda.bourial@gmail.com>
2020-09-23 02:23:01 +0200

    fixed bad test

--- inmemorylogger_test.go
+++ inmemorylogger_test.go
@@ -13,6 +13,6 @@ func TestInMemoryLogger(t *testing.T) {
 	loggedLines := buffer.StringArray(" ")
 	assert.Equal(t, len(loggedLines), 2)
 	assert.Contains(t, loggedLines[0], "[WARN]  ")
-	assert.Contains(t, loggedLines[0], "CET hello 1")
+	assert.Contains(t, loggedLines[0], "hello 1")
 	assert.Contains(t, loggedLines[1], "world 2")
 }
